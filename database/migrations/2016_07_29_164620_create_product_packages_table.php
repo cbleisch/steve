b142c3c8f64de68f6c9e0d72760a28094a68b051
Charles Bleisch <charkue@gmail.com>
2016-07-29 16:47:32 -0400

    Product package migration added.

---
+++ database/migrations/2016_07_29_164620_create_product_packages_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductPackagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_packages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->index();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('product_packages');
+    }
+}
